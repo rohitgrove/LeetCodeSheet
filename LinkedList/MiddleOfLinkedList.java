e46d6b94475f43a6f56339d7735504a835958236
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-08-21 16:10:49 +0530

    MiddleOfLinkedList

---
+++ LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,42 @@
+public class MiddleOfLinkedList {
+    public static Node middleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void printLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("Null");
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+
+        Node head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        printLL(head);
+        System.out.println(middleNode(head).val);
+    }
+}
