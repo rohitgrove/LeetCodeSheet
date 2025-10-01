de90aa43f656da7f263a33ba208dae017dd8e121
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-10-01 13:36:14 +0530

    TreeCreation

---
+++ BinaryTrees/ConstructTree.java
@@ -0,0 +1,17 @@
+public class ConstructTree {
+    public int idx = 0;
+
+    public TreeNode createTree(int data[]) {
+        if (data[idx] == -1) {
+            idx++;
+            return null;
+        }
+
+        TreeNode root = new TreeNode(data[idx++]);
+
+        root.left = createTree(data);
+        root.right = createTree(data);
+
+        return root;
+    }
+}
