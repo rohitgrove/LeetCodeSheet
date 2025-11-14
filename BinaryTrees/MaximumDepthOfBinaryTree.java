8bb19ea0f660673f30aad4cd244221798ae54583
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-11-14 13:01:29 +0530

    MaximumDepthOfBinaryTree

---
+++ BinaryTrees/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+public class MaximumDepthOfBinaryTree {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        ConstructTree ct = new ConstructTree();
+        int data[] = { 3, 9, -1, -1, 20, 15, -1, -1, 7, -1, -1 };
+        TreeNode root = ct.createTree(data);
+        ct.levelOrderTraversal(root);
+
+        System.out.println("Height Of Tree: " + maxDepth(root));
+    }
+}
