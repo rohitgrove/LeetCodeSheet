4d47f39424c1d7a61c52aa9bdafa91b082134360
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-11-14 09:43:01 +0530

    ConstructTreeAndTraversals

---
+++ BinaryTrees/TreeNode.java
@@ -0,0 +1,11 @@
+public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
