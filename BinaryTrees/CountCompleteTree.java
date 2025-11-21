59a9c309cdfa840baa6ff9a2d4d3986b7d4c3304
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-11-21 13:17:16 +0530

    CountCompleteTree

---
+++ BinaryTrees/CountCompleteTree.java
@@ -0,0 +1,29 @@
+public class CountCompleteTree {
+    public static void countNodesHelper(TreeNode root, int count[]) {
+        if (root == null) {
+            return;
+        }
+
+        countNodesHelper(root.left, count);
+        count[0]++;
+        countNodesHelper(root.right, count);
+    }
+
+    public static int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int count[] = { 0 };
+        countNodesHelper(root, count);
+        return count[0];
+    }
+
+    public static void main(String[] args) {
+        ConstructTree ct = new ConstructTree();
+        int data[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, -1 };
+        TreeNode root = ct.createTree(data);
+        ct.levelOrderTraversal(root);
+        System.out.println(countNodes(root));
+    }
+}
