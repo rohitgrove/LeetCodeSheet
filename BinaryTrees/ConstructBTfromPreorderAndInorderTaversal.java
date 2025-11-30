54935774090c6ebb65b3af44c22466ab6a07c800
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-11-30 12:59:11 +0530

    ConstructBTfromPreorderAndInorderTaversal

--- BinaryTrees/ConstructBTfromPreorderAndInorderTaversal.java
+++ BinaryTrees/ConstructBTfromPreorderAndInorderTaversal.java
@@ -45,7 +45,6 @@ public class ConstructBTfromPreorderAndInorderTaversal {
 
     public static void main(String[] args) {
         int preorder[] = { 3, 9, 20, 15, 7 }, inorder[] = { 9, 3, 15, 20, 7 };
-        ConstructTree ct = new ConstructTree();
-        ct.levelOrderTraversal(buildTree(preorder, inorder));
+        Traversals.levelOrderTraversal(buildTree(preorder, inorder));
     }
 }
