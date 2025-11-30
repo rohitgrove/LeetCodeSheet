f11f91b84ec27803fc553256c235f919a364b945
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-11-30 12:52:21 +0530

    ConstructTree

--- BinaryTrees/ConstructTree.java
+++ BinaryTrees/ConstructTree.java
@@ -1,6 +1,3 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class ConstructTree {
     public int idx = 0;
 
@@ -15,65 +12,5 @@ public class ConstructTree {
         root.left = createTree(data);
         root.right = createTree(data);
         return root;
-    }
-
-    public void preOrderTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        System.out.print(root.data + " ");
-        preOrderTraversal(root.left);
-        preOrderTraversal(root.right);
-    }
-
-    public void inOrderTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        inOrderTraversal(root.left);
-        System.out.print(root.data + " ");
-        inOrderTraversal(root.right);
-    }
-
-    public void postOrderTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        postOrderTraversal(root.left);
-        postOrderTraversal(root.right);
-        System.out.print(root.data + " ");
-    }
-
-    public void levelOrderTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        q.offer(null);
-
-        while (!q.isEmpty()) {
-            TreeNode topNode = q.poll();
-            if (topNode == null) {
-                System.out.println();
-                if (!q.isEmpty()) {
-                    q.offer(null);
-                }
-            } else {
-                System.out.print(topNode.data + " ");
-
-                if (topNode.left != null) {
-                    q.offer(topNode.left);
-                }
-
-                if (topNode.right != null) {
-                    q.offer(topNode.right);
-                }
-            }
-        }
     }
 }
