28cc87a0e99d7ae28b2fbda5392e611456cc3df2
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-02-07 08:37:13 +0530

    MinimumDeletionsToMakeStringBalanced

---
+++ DailyQuestionsOfLeetCode/MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,22 @@
+public class MinimumDeletionsToMakeStringBalanced {
+    public static int minimumDeletions(String s) {
+        int res = 0, b = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'b')
+                b++;
+            else if (b > 0) {
+                res++;
+                b--;
+            }
+        }
+
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(minimumDeletions("aababbab"));
+        System.out.println(minimumDeletions("bbaaaaabb"));
+    }
+}
