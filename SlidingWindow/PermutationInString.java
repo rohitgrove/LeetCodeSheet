40cab2098f64217ea3d911530081248adf15a232
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-02-11 12:50:05 +0530

    PermutationInString

---
+++ SlidingWindow/PermutationInString.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class PermutationInString {
+    public static boolean checkInclusion(String s1, String s2) {
+        s1 = sort(s1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length())))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String sort(String s) {
+        char[] t = s.toCharArray();
+        Arrays.sort(t);
+        return new String(t);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkInclusion("ab", "eidbaooo"));
+        System.out.println(checkInclusion("ab", "eidboaoo"));
+    }
+}
