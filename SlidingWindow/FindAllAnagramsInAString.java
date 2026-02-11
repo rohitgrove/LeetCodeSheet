14fb73b676f5cef96c1397ed9a077fdd48c72a89
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-02-11 12:41:46 +0530

    FindAllAnagramsInAString

---
+++ SlidingWindow/FindAllAnagramsInAString.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllAnagramsInAString {
+    public static boolean isAnagram(String s, String t) {
+        int freqTable[] = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            freqTable[s.charAt(i)]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            freqTable[t.charAt(i)]--;
+        }
+
+        for (int i = 0; i < freqTable.length; i++) {
+            if (freqTable[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        int start = 0;
+        int end = p.length();
+        System.out.println();
+        List<Integer> ans = new ArrayList<>();
+        while (end <= s.length()) {
+            if (isAnagram(s.substring(start, end), p)) {
+                ans.add(start);
+            }
+            start++;
+            end++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findAnagrams("cbaebabacd", "abc"));
+        System.out.println(findAnagrams("abab", "ab"));
+    }
+}
