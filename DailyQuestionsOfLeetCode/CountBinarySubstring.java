7b840570857c5ae74708514fabe2c6a4fbe07fd6
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-02-19 07:55:47 +0530

    CountBinarySubstring

---
+++ DailyQuestionsOfLeetCode/CountBinarySubstring.java
@@ -0,0 +1,20 @@
+public class CountBinarySubstring {
+    public static int countBinarySubstrings(String s) {
+        int res = 0, prev = 0, strk = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) strk++;
+            else {
+                prev = strk;
+                strk = 1;
+            }
+            if (strk <= prev) res++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countBinarySubstrings("00110011"));
+        System.out.println(countBinarySubstrings("10101"));
+    }
+}
