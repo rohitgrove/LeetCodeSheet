adfc05e58f4f44d9f84f0aeb8ddfd30c434a1282
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-03-02 19:28:00 +0530

    SpiralMatrix2

--- Matrix/SpiralMatrix2.java
+++ Matrix/SpiralMatrix2.java
@@ -5,18 +5,28 @@ public class SpiralMatrix2 {
         int[][] arr = new int[n][n];
 
         while (left <= right && top <= bottom) {
-            for (int i = left; i <= right; i++) arr[top][i] = count++;
+            for (int i = left; i <= right; i++) {
+                arr[top][i] = count++;
+            }
             top++;
 
-            for (int i = top; i <= bottom; i++) arr[i][right] = count++;
+            for (int i = top; i <= bottom; i++) {
+                arr[i][right] = count++;
+            }
             right--;
 
-            if (top <= bottom)
-                for (int i = right; i >= left; i--) arr[bottom][i] = count++;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    arr[bottom][i] = count++;
+                }
+            }
             bottom--;
 
-            if (left <= right)
-                for (int i = bottom; i >= top; i--) arr[i][left] = count++;
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    arr[i][left] = count++;
+                }
+            }
             left++;
         }
 
