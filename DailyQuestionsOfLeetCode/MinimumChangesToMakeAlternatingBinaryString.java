76385a3245c170aaa578416f9f1747b25f0a84a2
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-03-05 10:25:23 +0530

    MinimumChangesToMakeAlternatingBinaryString

---
+++ DailyQuestionsOfLeetCode/MinimumChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,26 @@
+public class MinimumChangesToMakeAlternatingBinaryString {
+    public static int minOperations(String s) {
+        int curr1 = 0;
+        int curr2 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char expected1 = (i % 2 == 0) ? '0' : '1';
+            char expected2 = (i % 2 == 0) ? '1' : '0';
+
+            if (s.charAt(i) != expected1) {
+                curr1++;
+            }
+            if (s.charAt(i) != expected2) {
+                curr2++;
+            }
+        }
+
+        return Math.min(curr1, curr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minOperations("0100"));
+        System.out.println(minOperations("10"));
+        System.out.println(minOperations("1111"));
+    }
+}
