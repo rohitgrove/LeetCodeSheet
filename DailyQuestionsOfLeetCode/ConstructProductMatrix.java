4cc536f7de9517a8f2260ae9a7ce19e301d367c2
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-03-25 09:23:50 +0530

    ConstructProductMatrix

--- DailyQuestionsOfLeetCode/ConstructProductMatrix.java
+++ DailyQuestionsOfLeetCode/ConstructProductMatrix.java
@@ -25,9 +25,9 @@ public class ConstructProductMatrix {
     }
 
     public static void printArr(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                System.out.print(grid[i][j] + " ");
+        for (int row[] : grid) {
+            for (int ele : row) {
+                System.out.print(ele + " ");
             }
             System.out.println();
         }
