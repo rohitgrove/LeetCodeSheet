a46280709d8fa77a737f31f5d7318fb80d2a28ca
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-03-28 09:54:28 +0530

    TopKFrequentElements

---
+++ Heap/TopKFrequentElements/Pair.java
@@ -0,0 +1,9 @@
+public class Pair {
+    public int element;
+    public int count;
+
+    public Pair(int element, int count) {
+        this.element = element;
+        this.count = count;
+    }
+}
