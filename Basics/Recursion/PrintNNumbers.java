b5679bcfa5133dddd8cbf32a85b2f1eadf64d518
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-04-21 12:48:02 +0530

    PrintNNumbers

---
+++ Basics/Recursion/PrintNNumbers.java
@@ -0,0 +1,14 @@
+public class PrintNNumbers {
+    public static void printNumbers(int current, int N) {
+        if (current > N) {
+            return;
+        }
+
+        System.out.print(current + " ");
+        printNumbers(current + 1, N);
+    }
+
+    public static void main(String[] args) {
+        printNumbers(1, 10);
+    }
+}
