e26ef523f9e8825c9470f3189e9c94421b8151d1
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-05-01 09:36:05 +0530

    PascalTriangle

--- Arrays/Hard/PascalTriangle.java
+++ Arrays/Hard/PascalTriangle.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
-    public static List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate1(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
         ans.add(new ArrayList<>());
         ans.get(0).add(1);
@@ -13,18 +13,48 @@ public class PascalTriangle {
         int row = 1;
         while (row < numRows) {
             int col = 0;
-            List<Integer> currAns = new ArrayList<>();
+            List<Integer> currRow = new ArrayList<>();
             while (col <= row) {
                 if (col == 0 || col == row) {
-                    currAns.add(1);
+                    currRow.add(1);
                 } else {
                     int firstEle = ans.get(row - 1).get(col - 1);
                     int secondEle = ans.get(row - 1).get(col);
-                    currAns.add(firstEle + secondEle);
+                    currRow.add(firstEle + secondEle);
                 }
                 col++;
             }
-            ans.add(currAns);
+            ans.add(currRow);
+            row++;
+        }
+
+        return ans;
+    }
+
+    public static List<List<Integer>> generate2(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        ans.get(0).add(1);
+        if (numRows == 1) {
+            return ans;
+        }
+
+        int row = 1;
+        while (row < numRows) {
+            int col = 0;
+            List<Integer> currRow = new ArrayList<>();
+            List<Integer> prevRow = ans.get(row - 1);
+            while (col <= row) {
+                if (col == 0 || col == row) {
+                    currRow.add(1);
+                } else {
+                    int firstEle = prevRow.get(col - 1);
+                    int secondEle = prevRow.get(col);
+                    currRow.add(firstEle + secondEle);
+                }
+                col++;
+            }
+            ans.add(currRow);
             row++;
         }
 
@@ -32,6 +62,6 @@ public class PascalTriangle {
     }
 
     public static void main(String[] args) {
-        System.out.println(generate(5));
+        System.out.println(generate1(5));
     }
 }
