795f99fd3908a03f782b1f3b13e02082ac3e8c1b
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-05-05 09:37:59 +0530

    SearchInRotatedSortedArray2

---
+++ BinarySearch/SearchOn1DArray/SearchInRotatedSortedArray2.java
@@ -0,0 +1,38 @@
+public class SearchInRotatedSortedArray2 {
+    public static boolean search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[low] == nums[mid]) {
+                low++;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 2, 5, 6, 0, 0, 1, 2 };
+        System.out.println(search(nums, 0));
+        System.out.println(search(nums, 3));
+    }
+}
