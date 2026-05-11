f88bccc01a3f2f72aca55a1062510cf0c9df5492
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-05-11 06:35:08 +0530

    SearchIn2Dmatrix2

---
+++ BinarySearch/SearchOn2dArrays/SearchIn2Dmatrix2.java
@@ -0,0 +1,27 @@
+public class SearchIn2Dmatrix2 {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (target > matrix[row][col]) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 4, 7, 11, 15 },
+                { 2, 5, 8, 12, 19 },
+                { 3, 6, 9, 16, 22 },
+                { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+
+        System.out.println(searchMatrix(matrix, 5));
+        System.out.println(searchMatrix(matrix, 20));
+    }
+}
