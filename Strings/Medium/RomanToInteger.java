fca33e3f73f94863d0ae7ea7490eed497d9b5230
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-05-14 15:12:58 +0530

    RomanToInteger

---
+++ Strings/Medium/RomanToInteger.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> roman = new HashMap<>();
+
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int res = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res -= roman.get(s.charAt(i));
+            } else{
+                res += roman.get(s.charAt(i));
+            }
+        }
+
+        return res + roman.get(s.charAt(s.length() - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+}
