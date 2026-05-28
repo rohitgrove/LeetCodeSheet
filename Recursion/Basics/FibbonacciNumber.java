70ed51f472eebc4d3940681c3e6f96cebb3c9772
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-05-28 06:56:36 +0530

    FibbonacciNumber

---
+++ Recursion/Basics/FibbonacciNumber.java
@@ -0,0 +1,14 @@
+public class FibbonacciNumber {
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(2));
+        System.out.println(fib(3));
+    }
+}
