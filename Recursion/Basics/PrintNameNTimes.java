2c980b6d702831297bb3c8cf26f9c0b1e47b7f94
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-05-28 06:08:36 +0530

    PrintNameNTimes

---
+++ Recursion/Basics/PrintNameNTimes.java
@@ -0,0 +1,13 @@
+public class PrintNameNTimes {
+    public static void printName(int count, int n, String name) {
+        if (count > n) {
+            return;
+        }
+
+        System.out.println(count + ". " + name);
+        printName(count + 1, n, name);
+    }
+    public static void main(String[] args) {
+        printName(1, 5, "Rohit");
+    }
+}
