e71e1c7213469c11c9a852989f67b0ab886216a6
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-06-08 08:50:15 +0530

    Pow

--- Recursion/GetAStrongHold/Pow.java
+++ Recursion/GetAStrongHold/Pow.java
@@ -21,8 +21,35 @@ public class Pow {
         return ans;
     }
 
+    public static double power(double x, long n) {
+        // Base case: anything raised to 0 is 1
+        if (n == 0) {
+            return 1.0;
+        }
+
+        // Base case: anything raised to 1 is itself
+        if (n == 1) {
+            return x;
+        }
+
+        // If 'n' is even
+        if (n % 2 == 0) {
+            // Recursive call: square the base and halve the exponent
+            return power(x * x, n / 2);
+        }
+
+        // If 'n' is odd
+        // Recursive call: multiply base once and reduce exponent by 1
+        return x * power(x, n - 1);
+    }
+
     public static double myPow(double x, int n) {
-        return bruteForce(x, n);
+        // return bruteForce(x, n);
+        if (n < 0) {
+            return 1.0 / power(x, -n);
+        }
+        // If 'n' is non-negative
+        return power(x, n);
     }
 
     public static void main(String[] args) {
