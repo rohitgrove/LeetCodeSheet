db94881f108d9d9b832a2047e1f9fdca49f53920
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-07-09 14:19:11 +0530

    MiddleOfLinkedList

---
+++ LinkedList/MediumOfLinkedList/LinkedList.java
@@ -0,0 +1,10 @@
+public class LinkedList {
+    public static void printLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+}
