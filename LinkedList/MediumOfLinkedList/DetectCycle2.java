15afef4f38702f16d7fb53c5f721ac58a13f93d1
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-07-20 15:14:25 +0530

    DetectCycle2

---
+++ LinkedList/MediumOfLinkedList/DetectCycle2.java
@@ -0,0 +1,40 @@
+public class DetectCycle2 {
+    public static Node detectCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (fast != slow) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(3);
+        Node node2 = new Node(2);
+        Node node3 = new Node(0);
+        Node node4 = new Node(-4);
+
+        Node head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+        System.out.println(detectCycle(head).data);
+    }
+}
