3a8c3f245f0c79b6c6f85428bcdff1f7949af762
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-08-02 09:22:14 +0530

    StoneGame1

---
+++ DailyQuestionsOfLeetCode/StoneGame1.java
@@ -0,0 +1,12 @@
+public class StoneGame1 {
+    public static boolean stoneGame(int[] piles) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int piles1[] = { 5, 3, 4, 5 };
+        System.out.println(stoneGame(piles1));
+        int piles2[] = { 3, 7, 3, 3 };
+        System.out.println(stoneGame(piles2));
+    }
+}
