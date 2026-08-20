7c7e942548c7850fc70399bf0d2134820bdfebd0
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-08-20 16:08:06 +0530

    RemoveList

--- Strings/Medium/SortCharacterByFrequemcy/PriorityQueueApproach/Pair.java
+++
@@ -1,9 +0,0 @@
-public class Pair {
-    public char character;
-    public int count;
-
-    public Pair(char character, int count) {
-        this.character = character;
-        this.count = count;
-    }
-}
