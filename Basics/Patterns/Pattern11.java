815fa120355d305f64b4a977cc83e10e5d5acb97
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-08-21 08:57:16 +0530

    Pattern11

---
+++ Basics/Patterns/Pattern11.java
@@ -0,0 +1,32 @@
+public class Pattern11 {
+	public static void main(String[] args) {
+		int n = 5;
+		int row = 1;
+		int tots_st = 1;
+		int tots_sp = n - 1;
+
+		while (row <= n) {
+			// space
+			int cnt_sp = 0;
+			while (cnt_sp < tots_sp) {
+				System.out.print("  ");
+				cnt_sp++;
+			}
+			// star!
+			int cnt_st = 0;
+			while (cnt_st < tots_st) {
+				if (cnt_st % 2 == 0) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+				cnt_st++;
+			}
+
+			System.out.println();
+			tots_st = tots_st + 2;
+			tots_sp--;
+			row++;
+		}
+	}
+}
