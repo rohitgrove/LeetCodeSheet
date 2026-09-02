3ab7f6f7e599842ed3c47cbb4090abf4b701c24d
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-09-02 10:58:14 +0530

    ConstructUniformParityArray1

---
+++ DailyQuestionsOfLeetCode/ConstructUniformParityArray1.java
@@ -0,0 +1,12 @@
+public class ConstructUniformParityArray1 {
+    public static boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = { 2, 3 };
+        System.out.println(uniformArray(nums1));
+        int nums2[] = { 4, 6 };
+        System.out.println(uniformArray(nums2));
+    }
+}
