515512c30678c38944f030d480e7445f3b2a08b1
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-09-08 10:15:36 +0530

    SearchInARotatedAndSortedArray2

--- BinarySearch/BSon1dArray/SearchInARotatedAndSortedArray2.java
+++ BinarySearch/BSon1dArray/SearchInARotatedAndSortedArray2.java
@@ -4,7 +4,9 @@ public class SearchInARotatedAndSortedArray2 {
 
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] == target) return true;
+            if (nums[mid] == target) {
+                return true;
+            }
 
             if (nums[low] == nums[mid]) {
                 low++;
@@ -12,18 +14,24 @@ public class SearchInARotatedAndSortedArray2 {
             }
 
             if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
-                else low = mid + 1;
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             } else {
-                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
-                else high = mid - 1;
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int nums[] = {2,5,6,0,0,1,2};
+        int nums[] = { 2, 5, 6, 0, 0, 1, 2 };
         System.out.println(search(nums, 0));
         System.out.println(search(nums, 3));
     }
