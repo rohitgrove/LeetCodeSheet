868eb71cc0da4c2b6b32cf0166ba054cea737729
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-09-08 10:45:41 +0530

    FindMinimumInRotatedSortedArray

--- BinarySearch/BSon1dArray/FindMinimumInRotatedSortedArray.java
+++ BinarySearch/BSon1dArray/FindMinimumInRotatedSortedArray.java
@@ -1,31 +1,34 @@
 public class FindMinimumInRotatedSortedArray {
-    public static int findPivotIdx(int nums[]) {
+    public static int findMin(int nums[]) {
         int start = 0;
         int end = nums.length - 1;
 
-        int mid = start + (end - start) / 2;
-
         while (start <= end) {
-            if (start == end) {
-                return start;
+            if (nums[start] <= nums[end]) {
+                return nums[start];
             }
-            if (mid + 1 < nums.length && nums[mid] > nums[mid + 1]) {
-                return mid + 1;
-            } else if (mid - 1 >= 0 && nums[mid] < nums[mid - 1]) {
-                return mid;
-            } else if (nums[start] > nums[mid]) {
-                end = mid - 1;
-            } else {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            } else if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            } else if (nums[mid] >= nums[start]) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            mid = start + (end - start) / 2;
         }
 
         return -1;
     }
 
     public static void main(String[] args) {
-        int nums[] = { 12, 14, 16, 2, 4, 6, 8, 10 };
-        System.out.println(findPivotIdx(nums));
+        int nums1[] = { 3, 4, 5, 1, 2 };
+        System.out.println(findMin(nums1));
+        int nums2[] = { 4, 5, 6, 7, 0, 1, 2 };
+        System.out.println(findMin(nums2));
+        int nums3[] = { 11, 13, 15, 17 };
+        System.out.println(findMin(nums3));
     }
 }
